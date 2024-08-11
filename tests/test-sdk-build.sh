b12a482cda97c47b7e3168181c1d5421bd1479d5
Nikhil Dabas <nikhil@nikhildabas.com>
2024-08-11 23:05:25 +0100

    Add RISC-V support

--- tests/test-sdk-build.sh
+++ tests/test-sdk-build.sh
@@ -2,13 +2,6 @@
 
 set -euxo pipefail
 
-COMMON_BUILD_PARAMS=(-G Ninja -DCMAKE_BUILD_TYPE=Debug --fresh)
-
-mkdir -p /tmp/pico-sdk-build
-cd /tmp/pico-sdk-build
-cmake "$PICO_SDK_PATH" -DPICO_SDK_TESTS_ENABLED=1 "${COMMON_BUILD_PARAMS[@]}"
-cmake --build .
-
 for REPO in examples extras; do
     DEST="/tmp/pico-${REPO}"
     export "PICO_${REPO^^}_PATH=${DEST}"
@@ -28,26 +21,28 @@ if [ ! -d "$FREERTOS_KERNEL_PATH" ]; then
         "$FREERTOS_KERNEL_PATH"
 fi
 
-cd "$PICO_EXAMPLES_PATH"
-
-# pico2 build disabled: https://github.com/raspberrypi/pico-examples/issues/513
-for board in pico
-do
-    mkdir -p build_$board
-    pushd build_$board
-    cmake .. -DPICO_BOARD=$board "${COMMON_BUILD_PARAMS[@]}"
+test-build () {
+    BUILD_DIR="$1"
+    shift 1
+    mkdir -p "$BUILD_DIR"
+    pushd "$BUILD_DIR"
+    cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Debug --fresh "$@"
     cmake --build .
     popd
-done
+}
 
-mkdir -p build_pico_w
-pushd build_pico_w
-cmake .. \
+cd "$PICO_SDK_PATH"
+test-build build -DPICO_SDK_TESTS_ENABLED=1
+
+cd "$PICO_EXAMPLES_PATH"
+
+test-build build-pico -DPICO_BOARD=pico
+# pico2 build disabled: https://github.com/raspberrypi/pico-examples/issues/513
+# test-build build-pico2 -DPICO_BOARD=pico2
+test-build build-pico2-riscv -DPICO_BOARD=pico2 -DPICO_PLATFORM=rp2350-riscv
+test-build build-pico_w \
     -DPICO_BOARD=pico_w \
     -DWIFI_SSID=ssid \
     -DWIFI_PASSWORD=pass \
     "-DFREERTOS_KERNEL_PATH=${FREERTOS_KERNEL_PATH}" \
-    -DTEST_TCP_SERVER_IP=10.10.10.10 \
-    "${COMMON_BUILD_PARAMS[@]}"
-cmake --build .
-popd
+    -DTEST_TCP_SERVER_IP=10.10.10.10
